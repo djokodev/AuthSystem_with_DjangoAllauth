c5347847501124dfe6d827e9b60b942a58dcd505
M@teo22 <djokodev75@gmail.com>
2024-07-22 04:30:05 +0200

    refactor: decoupage and enhancement of the loginView

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,9 @@
 
 from django.urls import path
-from authentication.views import loginPage, home, logout_view
+from authentication.views import loginView, home, logout_view
 
 urlpatterns = [
-  path("", loginPage, name="login"),
+  path("", loginView, name="login"),
   path("home/", home, name="home"),
   path("logout/", logout_view, name="logout"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -2,31 +2,58 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.contrib.auth import logout
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
-def loginPage(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
 
-        if not email or not password:
-            return render(request, 'login.html', {'error': 'Veuillez remplir tous les champs.'})
+def get_user_data(request):
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    return email, password
+
+def validate_login_form(email, password):
+    if not email or not password:
+        return 'Veuillez remplir tous les champs.'
+    try:
+        validate_email(email)
+    except ValidationError:
+        return 'Veuillez entrer une adresse email valide.'
+
+    return None
+
+
+def login_user(request, user, backend='django.contrib.auth.backends.ModelBackend'):
+    login(request, user, backend=backend)
+    return redirect('home')
+
+
+def create_user(email, password):
+    user = User.objects.create_user(username=email, email=email, password=password)
+    return user
+
+
+def loginView(request):
+    if request.method == 'POST':
+        email, password = get_user_data(request)
+
+        error_login_form = validate_login_form(email, password)
+        if error_login_form:
+            return render(request, 'login.html', {'error': error_login_form})
         
         try:
             user = authenticate(request, username=email, password=password, backend='django.contrib.auth.backends.ModelBackend')
-        except:
-            return print("Erreur d'authentification de l'utilisateur ", email)
+        except Exception as e:
+            return render(request, 'login.html', {'error': f"Erreur d'authentification : {str(e)}"})
 
         if user is not None:
-           login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-           return redirect('home')
+          return login_user(request, user)
         else:
-           user = User.objects.create(username=email, email=email, password=password)
-           if user:
-                login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-                return redirect('home')
-
+          user = create_user(email, password)
+          return login_user(request, user)
+          
     return render(request, "login.html")
+
 
 @login_required
 def home(request):
